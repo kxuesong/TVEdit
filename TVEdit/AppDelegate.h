d1d87bb3cab18ccc839847d9647e53331f2f12f5
小屁孩 <darknesswz@baramatoMacBook-Pro.local>
2014-09-03 19:38:03 +0800

    Initial Commit

---
+++ TVEdit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TVEdit
+//
+//  Created by 小屁孩 on 14-9-3.
+//  Copyright (c) 2014年 特普. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TVEdit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TVEdit
+//
+//  Created by 小屁孩 on 14-9-3.
+//  Copyright (c) 2014年 特普. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
